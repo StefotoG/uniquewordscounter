4963b8ed332fbb2e1c0ed942688122fe41b4b984
StefotoG <stefan.grozev@develiot.com>
2021-10-31 01:34:15 +0300

    one more test and remove build from repo

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stefan/uniquewordscounter
-# Build directory: /home/stefan/uniquewordscounter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/stefan/uniquewordscounter/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
-subdirs("test")
--- test/fileOperators.cpp
+++ test/fileOperators.cpp
@@ -6,4 +6,5 @@ extern "C" {
 
 TEST(sum, ok){
     ASSERT_EQ(sum(2, 3), 5);
+    ASSERT_EQ(sum(1,2),3);
 }
